fc9a2f34998120fe2aac8d947f346d2d5e1011b4
revanthkiran96 <116859708+revanthkiran96@users.noreply.github.com>
2023-05-22 10:26:28 +0530

    Update vpc.1.tf

--- vpc.1.tf
+++ vpc.1.tf
@@ -1,83 +1,99 @@
-resource "aws_vpc" "main" { #this name is belongs to terraform only
-    cidr_block = "10.0.0.0/16"
-    instance_tenancy = "default"
+resource "aws_vpc" "main" { #this name belongs to only terraform reference
 
+    cidr_block       = "10.0.0.0/16"
+    instance_tenancy = "default"
     tags = {
-      Name = "automated_vpc" #this name is belongs to aws
+        Name = "automated-vpc" #this name belongs to AWS
+    }
 }
-}
+
 resource "aws_subnet" "main" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.1.0/24"
-
+    #availability_zone ="us-east-1a"
+    
+    
+    
     tags = {
-        Name = "public_subnet_automated_vpc"
+      Name = "public-subnet-automatedvpc"
     }
   
 }
+
 resource "aws_subnet" "private" {
     vpc_id = aws_vpc.main.id
     cidr_block = "10.0.2.0/24"
 
     tags = {
-      Name = "private_subnet_automated_vpc"
+        Name = "private-subnet-automtedvpc"
     }
   
 }
-resource "aws_internet_gateway" "automated_igw" {
+
+resource "aws_internet_gateway" "automated-igw" {
     vpc_id = aws_vpc.main.id
 
     tags = {
-      Name  = "automated_igw"
+      Name = "automated-igw"
     }
   
 }
-resource "aws_route_table" "public_rt" {
+
+resource "aws_route_table" "public-rt" {
     vpc_id = aws_vpc.main.id
+
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.automated_igw.id
+        gateway_id = aws_internet_gateway.automated-igw.id
     }
+
     tags = {
-      Name  = "public_rt"
+      Name = "public-rt"
     }
   
 }
-resource "aws_eip" "auto_elp" {
+
+resource "aws_eip" "auto-eip" {
+    tags = {
+      Name = "auto-eip"
+    }
   
 }
-resource "aws_nat_gateway" "example" {
-  allocation_id = aws_eip.auto_elp.id
-  subnet_id     = aws_subnet.main.id
 
-  tags = {
-    Name = "gw NAT"
-  }
+resource "aws_nat_gateway" "auto-nat" {
+    allocation_id = aws_eip.auto-eip.id
+    subnet_id = aws_subnet.main.id
 
-  # To ensure proper ordering, it is recommended to add an explicit dependency
-  # on the Internet Gateway for the VPC.
- # depends_on = [aws_internet_gateway.example]
+    tags = {
+      Name = "auto-nat"
+    }
+
+    depends_on = [ aws_internet_gateway.automated-igw ]
+  
 }
 
-resource "aws_route_table" "private_rt" {
+resource "aws_route_table" "private-rt" {
     vpc_id = aws_vpc.main.id
+
     route {
         cidr_block = "0.0.0.0/0"
-        gateway_id = aws_nat_gateway.example.id
-
+        gateway_id = aws_nat_gateway.auto-nat.id
     }
 
     tags = {
-      Name = "private_rt"
+      Name = "private-rt"
     }
   
 }
-resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.main.id
-  route_table_id = aws_route_table.public_rt.id
+
+resource "aws_route_table_association" "public-subnet" {
+    subnet_id = aws_subnet.main.id
+    route_table_id = aws_route_table.public-rt.id
+  
 }
 
-resource "aws_route_table_association" "private" {
-  subnet_id      = aws_subnet.private.id
-  route_table_id = aws_route_table.private_rt.id
+resource "aws_route_table_association" "private-subnet" {
+    subnet_id = aws_subnet.private.id
+    route_table_id = aws_route_table.private-rt.id
+  
 }
